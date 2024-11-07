6e542fc6cb8ad41c95fa79aaa81daa9c782322af
serendipity <sju0924@khu.ac.kr>
2024-11-07 18:16:17 +0900

    Delete MemManage_Handler to inplement at runtime

--- stm32/Core/Src/stm32l5xx_it.c
+++ stm32/Core/Src/stm32l5xx_it.c
@@ -94,21 +94,6 @@ void HardFault_Handler(void)
 }
 
 /**
-  * @brief This function handles Memory management fault.
-  */
-void MemManage_Handler(void)
-{
-  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
-
-  /* USER CODE END MemoryManagement_IRQn 0 */
-  while (1)
-  {
-    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
-    /* USER CODE END W1_MemoryManagement_IRQn 0 */
-  }
-}
-
-/**
   * @brief This function handles Prefetch fault, memory access fault.
   */
 void BusFault_Handler(void)
