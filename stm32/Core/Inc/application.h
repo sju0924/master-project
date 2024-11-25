6bfcfa7cde98616d68061ca22e9593ab71daae0a
serendipity <sju0924@khu.ac.kr>
2024-11-25 16:10:06 +0900

    Update merge file

--- stm32/Core/Inc/application.h
+++ stm32/Core/Inc/application.h
@@ -2,10 +2,11 @@ void test_uart_print();
 void test_heap_allocation();
 void test_buffer_overflow();
 void test_buffer_underflow();
-void application();
 void test_heap_overflow();
 void test_heap_underflow();
 void test_gv_overflow();
 void test_gv_underflow();
 void test_nullptr_derefence();
 void test_UAF();
+
+void application();
